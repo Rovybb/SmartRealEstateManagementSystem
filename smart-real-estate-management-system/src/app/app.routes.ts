51e5588ab06e5fb2e45fb7ba3264a93ae60c59de
Rovybb <rbabalean@gmail.com>
2024-12-03 00:17:12 +0200

    PLEASEEEEE

--- smart-real-estate-management-system/src/app/app.routes.ts
+++ smart-real-estate-management-system/src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { PropertyCreateComponent } from './components/property-create/property-c
 import { PropertyUpdateComponent } from './components/property-update/property-update.component';
 
 export const appRoutes: Routes = [
-  { path: '', redirectTo: '/properties', pathMatch: 'full' },
+  { path: '', component: PropertyListComponent, pathMatch: 'full' },
   { path: 'properties', component: PropertyListComponent },
   { path: 'properties/create', component: PropertyCreateComponent },
   { path: 'properties/update/:id', component: PropertyUpdateComponent }
