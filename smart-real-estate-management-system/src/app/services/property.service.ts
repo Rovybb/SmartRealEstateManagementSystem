f7a47676b288f01a5ba77ed5d972e0d6b6c0604e
marcu <marcupetraru03@gmail.com>
2025-01-11 18:38:43 +0200

    added in frontend filters and inquiry

--- smart-real-estate-management-system/src/app/services/property.service.ts
+++ smart-real-estate-management-system/src/app/services/property.service.ts
@@ -12,30 +12,53 @@ import { environment } from '../../environments/environment';
 
     //private apiURL = 'https://smartrealestatemanagementsystem-d7edeaecc4faccgx.polandcentral-01.azurewebsites.net/api/v1/Properties';
     private apiURL = environment.API_URL + '/Properties';
+    private inqURL = 'https://localhost:7146/api/v1/Inquiries'
     constructor(private http: HttpClient) { }
 
   public getProperties(): Observable<Property[]> {
     return this.http.get<Property[]>(this.apiURL);
   }
 
+  public sendInquiry(
+    propertyId: string,
+    message: string,
+    status: number,
+    agentId: string,
+    clientId: string
+  ): Observable<any> {
+    const body = {
+      propertyId: propertyId,
+      message: message,
+      status: status,
+      agentId: agentId,
+      clientId: clientId
+    };
+
+      const token = this.getTokenFromCookies();
+      // Setează header-ul cu token-ul JWT
+      const headers = { Authorization: `Bearer ${token}` };
+  
+      return this.http.post(this.inqURL, body, { headers });
+  }
+
   public getPropertiesWithPagination(
     pageNumber: number,
     pageSize: number,
-    filters: { [key: string]: string } = {}
+    filters: { [key: string]: string | number }
   ): Observable<any> {
     let params = new HttpParams()
       .set('pageNumber', pageNumber.toString())
       .set('pageSize', pageSize.toString());
-
+  
     for (const key in filters) {
-      if (filters[key]) {
-        params = params.set(key, filters[key]);
+      if (filters[key] !== null) {
+        params = params.set(key, filters[key].toString());
       }
     }
-
+  
     const token = this.getTokenFromCookies();
     const headers = { Authorization: `Bearer ${token}` };
-
+  
     return this.http.get<any>(this.apiURL, { params, headers });
   }
 
