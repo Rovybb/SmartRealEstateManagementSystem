f997209c8e683fa12f3520c5903f6bb37f714044
Rovybb <rbabalean@gmail.com>
2025-05-27 19:06:55 +0300

    fix: Update environment configuration for production

--- smart-real-estate-management-system/src/environments/environment.ts
+++ smart-real-estate-management-system/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    API_URL: 'https://smartrealestatemanagementsystem20250527175616.azurewebsites.net',
+    API_URL: 'https://smartrealestatemanagementsystem20250527175616.azurewebsites.net/api/v1',
   };
   
